51ae3f1b6058df118e37aec2bbd1647dfe055087
Jackson Kearl <jackson.kearl@apollographql.com>
2019-08-12 17:18:29 -0700

    Prepare for TS conversion

--- .babelrc.js
+++ .babelrc.js
@@ -1,18 +1,20 @@
-module.exports = {
-  presets: ['@babel/preset-env'],
-  plugins: [
-    './resources/inline-invariant',
-    '@babel/plugin-transform-flow-strip-types',
-    ['@babel/plugin-transform-classes', { loose: true }],
-    ['@babel/plugin-transform-destructuring', { loose: true }],
-    ['@babel/plugin-transform-spread', { loose: true }],
-  ],
-  env: {
-    cjs: {
-      presets: [['@babel/preset-env', { modules: 'commonjs' }]],
-    },
-    mjs: {
-      presets: [['@babel/preset-env', { modules: false }]],
-    },
-  },
-};
+// Ignore existing babel stuff during conversion to typescript
+module.exports = {};
+// module.exports = {
+//   presets: ['@babel/preset-env'],
+//   plugins: [
+//     './resources/inline-invariant',
+//     '@babel/plugin-transform-flow-strip-types',
+//     ['@babel/plugin-transform-classes', { loose: true }],
+//     ['@babel/plugin-transform-destructuring', { loose: true }],
+//     ['@babel/plugin-transform-spread', { loose: true }],
+//   ],
+//   env: {
+//     cjs: {
+//       presets: [['@babel/preset-env', { modules: 'commonjs' }]],
+//     },
+//     mjs: {
+//       presets: [['@babel/preset-env', { modules: false }]],
+//     },
+//   },
+// };
